01917aa968c825716be5159f74e1905b244a0f47
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-02-27 12:17:40 +0300

    fix: correct the solution
    Now its correct

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,14 +1,15 @@
 module.exports = function countCats(backyard) {
-  throw 'Not implemented';
-  var a = [];
-  backyard.forEach(function(index, array){
-    if(array[index] !== '^^') {
-      return 0;
-    } else if(array[index] === '^^') {
-      a.push(array[index]);
-      return a.length;
-    };
-  });
-    
-  };
+  var b = backyard.flat(Infinity);
+  var count = 0;
+  if(b.length === 0){
+    return 0;
+  } else {
+    for(var i = 0; i < b.length; i++){
+      if(b[i] === '^^') {
+        count++;
+      }
+    }
+  }  
+  return count;
+};
 
