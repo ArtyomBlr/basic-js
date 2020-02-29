1063b6d91daea9b76a63002162cb6d650eaae2c0
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-02-29 17:13:00 +0300

    init: start task dreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if(!Array.isArray(members)) {
+    return 'Error';
+  }
+
+  var dreamTeam = [];
+
+  for(var i = 0; i < members.length; i++){
+    if(typeof members[i] == 'string'){
+      var a = members[i].trim()[0].toUpperCase();
+      dreamTeam.push(a);
+    }
+  }
+  return dreamTeam.sort().join("");
 };
