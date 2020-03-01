78ef493c2b11ffe19ff1cdf7a14c25f23b2f3153
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-03-01 14:26:08 +0300

    feat: add function carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,13 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15; 
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+    if(typeof sampleActivity !== 'string' || Number(sampleActivity) > 15 || Number(sampleActivity) < 1 || isNaN(parseFloat(sampleActivity))){
+        return false;
+    }
+
+    var a = parseFloat(sampleActivity);
+    return Math.ceil(Math.log(MODERN_ACTIVITY / a) / (0.693 / HALF_LIFE_PERIOD));
+
 };
