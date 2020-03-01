0e5adc9b4e743b0b0492362b04915b47c345744a
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-03-01 18:56:03 +0300

    feat: add function getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,19 @@
 module.exports = function getSeason(/* date */) {
   throw 'Not implemented';
   // remove line with error and write your code here
+    if(!date){
+        return 'Unable to determine the time of year!';
+    }
+
+    let month = date.getUTCMonth();
+    if(month == 11 || month < 2){
+        return 'winter';
+    } else if(month <= 4 && month >= 2){
+        return 'spring';
+    } else if(month <= 7 && month >= 5){
+        return 'summer';
+    } else if(month <= 10 && month >= 8){
+        return 'autumn';
+    }
+  
 };
