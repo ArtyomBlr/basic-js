5e4d3e8628cc48c7ae3c1074d264bbf1210eedd3
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-03-07 17:46:21 +0300

    docs: change some syntax

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,13 +1,14 @@
 module.exports = class DepthCalculator {
     calculateDepth(arr) {
-            let i = 0;
-            while(arr.length){
-                 i++;
-                 arr = arr.reduce( (level, item) => {
-                      if(Array.isArray(item)) level++;
-                      return level;
-                 }, []);
-            }
-            return i;
-        }
+
+          let i = 0;  
+               while(arr.length){
+                    i++;
+                    arr = arr.reduce( (level, item) => {
+                         if(Array.isArray(item)) level++;
+                         return level;
+                    }, []);
+               }
+               return i;
+     }
 };
