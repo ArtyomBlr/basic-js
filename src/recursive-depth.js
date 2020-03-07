67788e642155f25d4bd760696d3f0c2012feed79
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-03-07 14:26:51 +0300

    feat: create function, somthing is incorrect

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,13 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+    calculateDepth(arr) {
+            let i = 0;
+            while(arr.length){
+                 i++;
+                 arr = arr.reduce( (level, item) => {
+                      if(Array.isArray(item)) level++;
+                      return level;
+                 }, []);
+            }
+            return i;
+        }
 };
